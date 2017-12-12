0f98fdbaf006e1d422d995e912abec2a9243b764
Joffrey Darcq <j-off@live.fr>
2017-12-12 12:24:49 +0100

    Update radicale-user.py

--- radicale-user.py
+++ radicale-user.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Usage exemple:
-    input  :./script username password
+"""radicale-user usage:
+    input  : radicale-user username
     output : username:bcrypthash
 
-Add to Htpasswd filename:
-    ./script username password >> /etc/radicale/users
+Redirect the output to yours user(s) file path:
+    radicale-user username >> /etc/radicale/users
 """
 
+import getpass
 import bcrypt
 import sys
 
 try:
-    password = bytes(sys.argv[2], 'utf-8')
+    user = sys.argv[1]
 except IndexError:
-    sys.exit('Usage: %s [username] [password]' % sys.argv[0])
-else:
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
-    print('%s:%s' % (sys.argv[1], hashed))
+    sys.exit(__doc__)
+
+password = getpass.getpass('Password: ')
+password = bytes(password, 'utf-8')
+hashed = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
+
+print(('{0}:{1}'.format(user, hashed)))
